a20de7829a9db75ce606d17f8ac51e26e80e12e0
d4nQw3rty <danlinmoy@gmail.com>
2022-11-21 17:02:46 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Home from './components.js/Home';
 import NavBar from './components.js/NavBar';
+import { fetchCountries } from './redux/reducer/countriesSlice';
 import './App.css';
 
-const App = () => (
-  <div>
-    <NavBar />
-    <Home />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  dispatch(fetchCountries());
+  return (
+    <>
+      <main>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
+  );
+};
 
 export default App;
