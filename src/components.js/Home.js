61f65d114e8c68e3e7a9deef118b9f041225bfde
d4nQw3rty <danlinmoy@gmail.com>
2022-11-23 10:56:28 -0400

    Set conditional rendering
    Conditional rendering for search input

--- src/components.js/Home.js
+++ src/components.js/Home.js
@@ -1,42 +1,67 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { searchUpdate } from '../redux/reducer/countriesSlice';
+import '../App.css';
 
 const img = require('../assets/continent.png');
 
 const Home = () => {
   const country = useSelector((state) => state.countries.countries);
+  const search = useSelector((state) => state.countries.search);
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    dispatch(searchUpdate(e.target.value));
+  };
 
   return (
     <div>
       <div className="continent">
         <div className="continent-img">
-          <img src={img} alt="continent" className="continent-img" />
+          <img src={img} alt="continent" />
         </div>
         <div className="continent-text">
           <h1 className="continent-title">South America</h1>
         </div>
       </div>
+      <div className="inputContainer">
+        <input value={search} type="text" onChange={handleChange} className="inputTex" />
+      </div>
       <div className="container">
-        {country.map((item) => (
-          <div className="card" key={item.name}>
-            <div className="card-image-container">
-              <img
-                className="card-image"
-                src={item.flag
-                            .replace('https://restcountries.com/data/', 'https://restcountries.com/data/')  // eslint-disable-line
-                  .replace('.svg', '.png')}
-                alt={item.name}
-              />
-            </div>
-            <div className="card-body">
-              <h2>{item.name}</h2>
-              <p>
-                <strong>Population:</strong>
-                {item.population}
-              </p>
-            </div>
-          </div>
-        ))}
+        {
+          search === ''
+            ? country.map((item) => (
+              <div className="card" key={item.name}>
+                <div className="card-img">
+                  <img
+                    src={item.flag
+                      ? item.flag
+                      : 'https://www.countryflags.io/xx/flat/64.png'}
+                    alt="flag"
+                  />
+                </div>
+                <div className="card-text">
+                  <h2 className="card-title">{item.name}</h2>
+                </div>
+              </div>
+            ))
+            : country.filter((item) => item.name
+              .toLowerCase()
+              .includes(search.toLowerCase())).map((item) => (
+                <div className="card" key={item.name}>
+                  <div className="card-img">
+                    <img
+                      src={item.flag
+                        ? item.flag
+                        : 'https://www.countryflags.io/xx/flat/64.png'}
+                      alt="flag"
+                    />
+                  </div>
+                  <div className="card-text">
+                    <h2 className="card-title">{item.name}</h2>
+                  </div>
+                </div>
+            ))
+        }
       </div>
     </div>
   );
