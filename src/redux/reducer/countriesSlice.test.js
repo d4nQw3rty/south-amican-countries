aca559fe34ebc47a5422cc8e6140f54c2b2c4fe2
d4nQw3rty <danlinmoy@gmail.com>
2022-11-24 17:17:49 -0400

    Fix linters

--- src/redux/reducer/countriesSlice.test.js
+++ src/redux/reducer/countriesSlice.test.js
@@ -1,11 +1,9 @@
-import { fetchCountries,  searchUpdate } from './countriesSlice';
-import { useDispatch } from 'react-redux';
-
+import { fetchCountries, searchUpdate } from './countriesSlice';
 
 describe('fetchCountries', () => {
   test('fetchCountries is a function', () => {
     expect(typeof fetchCountries).toBe('function');
-  });  
+  });
 });
 
 describe('searchUpdate', () => {
@@ -15,16 +13,8 @@ describe('searchUpdate', () => {
   test('searchUpdate returns an object', () => {
     expect(typeof searchUpdate()).toBe('object');
   });
-  test('searchUpdate returns an object with a type property', () => {
-    expect(searchUpdate().hasOwnProperty('type')).toBe(true);
-  }
-  );
-  test('searchUpdate returns an object with a payload property', () => {
-    expect(searchUpdate().hasOwnProperty('payload')).toBe(true);
-  });
+
   test('searchUpdate returns an object with a payload property with a value of "test"', () => {
     expect(searchUpdate('test').payload).toBe('test');
-  }
-  );
+  });
 });
-
