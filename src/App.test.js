9f61bc349825971abf3a517c2adcd45ca48b7a20
d4nQw3rty <danlinmoy@gmail.com>
2022-11-25 08:04:26 -0400

    Fix test fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App component', () => {
+    const component = renderer.create(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>,
+    );
+    expect(component).toMatchSnapshot();
+  });
 });
+
